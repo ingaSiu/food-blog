627d9a78864512bd2059434074b834e8fada2903
Inga <inga@local.machine>
2023-02-03 13:20:48 +0200

    changed some data

--- src/assets/recipe.js
+++ src/assets/recipe.js
@@ -7,4 +7,20 @@ export const recipes = [
     ingriedients: '4 porcijoms reikės: daaug ingredientu',
     making: 'Gaminame: visi astuoni steps',
   },
+  {
+    id: 2,
+    title: 'RYŽIŲ MAKARONAI SU KREMINIU RIEŠUTŲ SVIESTO PADAŽU',
+    imageUrl:
+      'https://i0.wp.com/antmedineslenteles.com/wp-content/uploads/2023/01/blog-IMG_7397-2.jpg?resize=720%2C720&ssl=1',
+    ingriedients: '4 porcijoms reikės: daaug ingredientu',
+    making: 'Gaminame: visi astuoni steps',
+  },
+  {
+    id: 3,
+    title: 'TANDOORI VIŠTIENA',
+    imageUrl:
+      'https://i0.wp.com/antmedineslenteles.com/wp-content/uploads/2023/01/blog-IMG_7346.jpg?resize=720%2C720&ssl=1',
+    ingriedients: '4 porcijoms reikės: daaug ingredientu',
+    making: 'Gaminame: visi astuoni steps',
+  },
 ];
