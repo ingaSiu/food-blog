fd0c40ebbc7cf2624ffc15074623fdfb9175752c
Inga <inga@local.machine>
2023-02-06 12:37:47 +0200

    fetch one post from api

--- src/pages/RecipePage.jsx
+++ src/pages/RecipePage.jsx
@@ -1,23 +1,39 @@
 import RecipeCard from '../components/RecipeCard';
-import { recipes } from '../assets/recipe';
+import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import { PageContainer } from '../components/styles/RecipeCard.styled';
 const RecipePage = () => {
+  const [recipeData, setRecipeData] = useState(undefined);
   const { recipeId } = useParams();
+  const getPost = () => {
+    axios
+      .get('http://localhost:3001/api/public/posts/' + recipeId)
+      .then((response) => {
+        setRecipeData(response.data);
+        console.log('response from single post ftech');
+        console.log(response.data);
+        console.log(recipeData);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  useEffect(() => {
+    getPost();
+  }, []);
 
   console.log(useParams());
   return (
     <>
       <PageContainer>
-        {recipes
-          .filter((recipe) => recipe.id === Number(recipeId))
-          .map((recipe) => (
+        {recipeData &&
+          recipeData.map((recipe) => (
             <RecipeCard
-              key={recipe.id}
+              key={recipe._id}
               title={recipe.title}
               imageUrl={recipe.imageUrl}
-              ingriedients={recipe.ingriedients}
-              making={recipe.making}
+              ingriedients={recipe.content}
+              making={recipe.content}
             ></RecipeCard>
           ))}
       </PageContainer>
