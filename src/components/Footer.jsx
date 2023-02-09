06c1cf6464f8a6acf3a173861d426e8d541ab3ff
ingaSiu <ingasiud@gmail.com>
2023-02-09 21:50:13 +0200

    Added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import { FooterContainer, FooterText } from './styles/Footer.styled';
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterText>COPYRIGHT © 2023 NEKOTASTIC KITCHEN</FooterText>
+    </FooterContainer>
+  );
+};
+export default Footer;
