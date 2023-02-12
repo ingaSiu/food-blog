1adc850dea8fb37adc139e3007563bce48b84c4b
ingaSiu <ingasiud@gmail.com>
2023-02-12 13:22:25 +0200

    Fetch items by category id

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,5 +1,41 @@
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { CategoryContext } from '../contexts/CategoryContext';
+import FoodCardMapping from './FoodCardMapping';
+import { PostsContainer } from '../components/styles/MainPage.styled';
+import { CategoryTitleContainer } from '../components/styles/FoodCard.styled';
+import { useContext } from 'react';
 const CategoryPage = () => {
-  return <div>Category</div>;
+  const [postsData, setPostsData] = useState([]);
+  const [categoryTitle, setCategoryTitle] = useState('');
+  const { categoryId } = useParams();
+  const { categories } = useContext(CategoryContext);
+  console.log(categories);
+  const getCategoryPosts = () => {
+    axios
+      .get(`http://localhost:3001/api/public/categories-posts/${categoryId}`)
+      .then((response) => {
+        setPostsData(response.data);
+        console.log('New data:');
+        console.log(response.data);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  useEffect(() => {
+    getCategoryPosts();
+    const category = categories.find((category) => category._id === categoryId);
+    setCategoryTitle(category.title);
+  }, [categoryId]); // eslint-disable-line react-hooks/exhaustive-deps
+  return (
+    <PostsContainer>
+      <CategoryTitleContainer categories={categories}>
+        <h1>Category: {categoryTitle}</h1>
+      </CategoryTitleContainer>
+      <FoodCardMapping posts={postsData} />
+    </PostsContainer>
+  );
 };
 
 export default CategoryPage;
