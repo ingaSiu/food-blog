0deacb999114bcf73f168037773b4ded39fd09e2
ingaSiu <ingasiud@gmail.com>
2023-02-13 11:42:17 +0200

    Added category name to table

--- src/components/AdminPageTables/PostsTable.jsx
+++ src/components/AdminPageTables/PostsTable.jsx
@@ -7,13 +7,27 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { useContext } from 'react';
-
+import { CategoryContext } from '../../contexts/CategoryContext';
 import { PostsContext } from '../../contexts/PostsContext';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import { useState, useEffect } from 'react';
 
 const PostsTable = ({ delIcon, editIcon }) => {
   const { posts } = useContext(PostsContext);
+  const { categories } = useContext(CategoryContext);
+  const [postsWithNames, setPostsWithNames] = useState([]);
+
+  useEffect(() => {
+    setPostsWithNames(
+      posts.map((post) => {
+        const category = categories.find((cat) => {
+          return post.categoryId === cat._id;
+        });
+        return { ...post, categoryTitle: category.title };
+      }),
+    );
+  }, [posts, categories]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <>
@@ -24,19 +38,21 @@ const PostsTable = ({ delIcon, editIcon }) => {
               <TableCell>Post title</TableCell>
               <TableCell align="center">Post&nbsp;ID</TableCell>
               <TableCell align="center">Category&nbsp;ID</TableCell>
+              <TableCell align="center">Category&nbsp;NAME</TableCell>
               <TableCell align="center">Edit</TableCell>
               <TableCell align="center"> Delete</TableCell>
             </TableRow>
           </TableHead>
 
           <TableBody>
-            {posts.map((post) => (
+            {postsWithNames.map((post) => (
               <TableRow key={post._id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
                 <TableCell component="th" scope="row">
                   {post.title}
                 </TableCell>
                 <TableCell align="right">{post._id}</TableCell>
                 <TableCell align="right">{post.categoryId}</TableCell>
+                <TableCell align="right">{post.categoryTitle}</TableCell>
                 <TableCell align="center">{(editIcon = <EditIcon />)}</TableCell>
                 <TableCell align="center">{(delIcon = <DeleteIcon />)}</TableCell>
               </TableRow>
