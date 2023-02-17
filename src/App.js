52eae31d745ff66d3f87c2cdc8ca2ff661e3bffc
ingaSiu <ingasiud@gmail.com>
2023-02-17 13:06:46 +0200

    Added react query

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Routes from './routes/Routes';
 import GlobalStyles from './components/styles/Global';
+import { QueryClientProvider, QueryClient } from 'react-query';
+
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyles />
       <Routes />
-    </>
+    </QueryClientProvider>
   );
 };
 
