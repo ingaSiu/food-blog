bc0d576428c083a5219639e5adaaebc81af2db99
ingaSiu <ingasiud@gmail.com>
2023-02-19 17:46:17 +0200

    fetching data

--- src/api/categories.js
+++ src/api/categories.js
@@ -1,3 +1,32 @@
 import axios from 'axios';
+import { BASE_PUBLIC_URL, BASE_ADMIN_URL } from './baseApi';
 
-// const CATEGORIES_API =
+const PUBLIC_CATEGORIES_URLS = {
+  getAllCategories: `${BASE_PUBLIC_URL}categories`,
+  getOneCategory: `${BASE_PUBLIC_URL}categories/`,
+  getPostsByCategory: `${BASE_PUBLIC_URL}categories-posts/`,
+};
+
+const ADMIN_CATEGORIES_URLS = {
+  insert: `${BASE_ADMIN_URL}categories`,
+  update: `${BASE_ADMIN_URL}categories/`,
+};
+
+export const getAllCategories = () => {
+  console.log('START get all categories');
+  console.log(PUBLIC_CATEGORIES_URLS.getAllCategories);
+  return axios.get(PUBLIC_CATEGORIES_URLS.getAllCategories).then((response) => {
+    console.log('SUCCESS getAllCategories');
+    console.log(response.data);
+    return response.data;
+  });
+};
+
+export const getPostsByCategory = ({ queryKey }) => {
+  const id = queryKey[1];
+  console.log('START get all posts by category');
+  console.log(PUBLIC_CATEGORIES_URLS.getPostsByCategory);
+  return axios.get(`${PUBLIC_CATEGORIES_URLS.getPostsByCategory}${id}`).then((response) => {
+    return response.data;
+  });
+};
