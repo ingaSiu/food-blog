8a1349d9892c529849b24d9f08f025bf0e813662
ingaSiu <ingasiud@gmail.com>
2023-02-19 18:13:26 +0200

    create category

--- src/pages/Admin/AdminCategories.jsx
+++ src/pages/Admin/AdminCategories.jsx
@@ -1,11 +1,32 @@
-import { TableWrapper } from '../../components/AdminPageTables/Table.styled';
+import { useState } from 'react';
+
+import { TableWrapper, TitleContainer } from '../../components/AdminPageTables/Table.styled';
 import CategoriesTable from '../../components/AdminPageTables/CategoriesTable';
+import ButtonMain from '../../components/Button';
+import { TablePageContainer } from '../../components/AdminPageTables/Table.styled';
+import CreateNewCategory from '../../components/AdminPageForms/PostCategoryForm';
+
 const AdminCategories = () => {
+  const [openForm, setOpenForm] = useState(false);
+
   return (
-    <TableWrapper>
-      <h1>Categories list:</h1>
-      <CategoriesTable></CategoriesTable>
-    </TableWrapper>
+    <TablePageContainer>
+      <TableWrapper>
+        <TitleContainer>
+          <h1>Categories list:</h1>
+
+          <ButtonMain
+            onClick={() => {
+              setOpenForm(true);
+            }}
+          >
+            Create a new post
+          </ButtonMain>
+        </TitleContainer>
+        <CategoriesTable></CategoriesTable>
+      </TableWrapper>
+      {openForm && <CreateNewCategory closeForm={setOpenForm} />}
+    </TablePageContainer>
   );
 };
 
