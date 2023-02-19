4ffc75a90774c64c6b5b112fa3067d8d9e8d1d48
ingaSiu <ingasiud@gmail.com>
2023-02-19 14:44:14 +0200

    Reusable base api url

---
+++ src/api/baseApi.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+//TODO take this from .env
+const BASE_URL = 'http://localhost:3001/api/';
+export const BASE_PUBLIC_URL = `${BASE_URL}public/`;
+export const BASE_ADMIN_URL = `${BASE_URL}admin/`;
