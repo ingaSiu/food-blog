ccccb99b72645c80978b30b256b1c39f93abbbad
ingaSiu <ingasiud@gmail.com>
2023-02-19 14:44:35 +0200

    added query client

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Routes from './routes/Routes';
 import GlobalStyles from './components/styles/Global';
 import { QueryClientProvider, QueryClient } from 'react-query';
 
-const queryClient = new QueryClient();
+export const queryClient = new QueryClient();
 
 const App = () => {
   return (
