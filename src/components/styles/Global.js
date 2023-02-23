7ccd0b0a3570b4f6017593bdf7d929cc67ba9737
ingaSiu <ingasiud@gmail.com>
2023-02-23 18:22:54 +0200

    added a global style

--- src/components/styles/Global.js
+++ src/components/styles/Global.js
@@ -15,6 +15,12 @@ body {
 h1 {
   margin: 0;
 }
+
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
 //https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,300&display=swap
 @font-face {
   font-family: 'Lato';
