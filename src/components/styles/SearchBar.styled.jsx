436599186a514f2d6bb0460d5a7195b4bdaad009
ingaSiu <ingasiud@gmail.com>
2023-02-23 18:23:18 +0200

    SearchBarComponent styling

--- src/components/styles/SearchBar.styled.jsx
+++ src/components/styles/SearchBar.styled.jsx
@@ -16,3 +16,20 @@ export const SearchInput = styled.input`
   outline: none;
   padding: 10px 40px;
 `;
+
+export const ContentContainer = styled.div`
+  height: 135px;
+  background-color: #ffffff;
+  border: 1px solid grey;
+  border-radius: 5px;
+  width: 225px;
+  overflow-y: auto;
+  margin-left: 144px;
+  margin-top: 5px;
+
+  p {
+    padding-left: 5px;
+
+    font-size: 15px;
+  }
+`;
