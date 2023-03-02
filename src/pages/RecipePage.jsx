a76477ff381ba1f71c18aa7e9ebbde0cdb704a1d
ingaSiu <ingasiud@gmail.com>
2023-03-02 16:29:54 +0200

    comments visible in recipe page

--- src/pages/RecipePage.jsx
+++ src/pages/RecipePage.jsx
@@ -1,14 +1,18 @@
-import RecipeCard from '../components/RecipeCard';
-import { useParams } from 'react-router-dom';
-
+import CommentCard from '../components/CommentCard';
+import { CommentsWrapper } from '../components/styles/CommentCard.styled';
 import { PageContainer } from '../components/styles/RecipeCard.styled';
-import { usePostQuery } from '../hooks/posts';
 import PostComment from '../components/AdminPageForms/PostComentForm';
+import RecipeCard from '../components/RecipeCard';
+import StarRating from '../components/StarRating';
+import { useGetCommentsByPostId } from '../hooks/comments';
+import { useParams } from 'react-router-dom';
+import { usePostQuery } from '../hooks/posts';
 
 const RecipePage = () => {
   const { recipeId } = useParams();
 
   const { data: recipeData } = usePostQuery(recipeId);
+  const { data: comments } = useGetCommentsByPostId(recipeId);
 
   return (
     <>
@@ -21,6 +25,20 @@ const RecipePage = () => {
             content={recipeData.content}
           ></RecipeCard>
         )}
+
+        <StarRating />
+        <CommentsWrapper>
+          {comments &&
+            comments.map((comment) => (
+              <CommentCard
+                key={comment._id}
+                userName={comment.userName}
+                comment={comment.content}
+                date={comment.createdAt}
+              />
+            ))}
+        </CommentsWrapper>
+
         <PostComment postId={recipeId} />
       </PageContainer>
     </>
