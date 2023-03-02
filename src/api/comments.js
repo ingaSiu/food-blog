8b27fc4be5565c67fa3014398adc8b501782b5ec
ingaSiu <ingasiud@gmail.com>
2023-03-02 16:28:39 +0200

    Get comments by post id axios

--- src/api/comments.js
+++ src/api/comments.js
@@ -1,19 +1,29 @@
+import { BASE_ADMIN_URL, BASE_PUBLIC_URL } from './baseApi';
+
 import axios from 'axios';
-import { BASE_PUBLIC_URL, BASE_ADMIN_URL } from './baseApi';
 
 const PUBLIC_COMMENTS_URLS = {
   getComments: `${BASE_PUBLIC_URL}comments`,
-  getCommentsByPostId: `${BASE_PUBLIC_URL}post-comments/:id`,
+  getCommentsByPostId: `${BASE_PUBLIC_URL}posts-comments/`,
   postComment: `${BASE_PUBLIC_URL}comments`,
 };
 
 const ADMIN_COMMENTS_URLS = {
   updateComment: `${BASE_ADMIN_URL}comments`,
-  deleteComment: `${BASE_ADMIN_URL}comments/:id`,
+  deleteComment: `${BASE_ADMIN_URL}comments/`,
 };
 
 export const getAllComments = () => {
   return axios.get(PUBLIC_COMMENTS_URLS.getComments).then((response) => {
+    return response.data;
+  });
+};
+
+export const getCommentsByPostId = ({ queryKey }) => {
+  const id = queryKey[1];
+  console.log('START get all comments by id');
+  console.log(`${PUBLIC_COMMENTS_URLS.getCommentsByPostId}${id}`);
+  return axios.get(`${PUBLIC_COMMENTS_URLS.getCommentsByPostId}${id}`).then((response) => {
     return response.data;
   });
 };
