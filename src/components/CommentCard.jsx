a2f9b6936ac455028753de5ebe86256ff7667046
ingaSiu <ingasiud@gmail.com>
2023-03-03 12:56:55 +0200

    comment card with date conversion

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,10 +1,25 @@
-import { CardContainer, Comment, Date, UserName } from './styles/CommentCard.styled';
+import { CardContainer, Comment, Date, UserContent, UserIcon, UserInfo, UserName } from './styles/CommentCard.styled';
 
 const CommentCard = ({ userName, date, comment }) => {
+  const commentDate = new Intl.DateTimeFormat('en-GB', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  }).format(date);
+
   return (
     <CardContainer>
-      <UserName>{userName} says:</UserName>
-      <Date>Posted at: {date}</Date>
+      <UserContent>
+        <UserIcon />
+        <UserInfo>
+          <UserName>{userName} says:</UserName>
+          <Date>{commentDate}</Date>
+        </UserInfo>
+      </UserContent>
+
       <Comment>{comment}</Comment>
     </CardContainer>
   );
