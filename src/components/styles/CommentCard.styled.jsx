4e306b5ed6226791f5015acaf6ebae32a4bc0fec
ingaSiu <ingasiud@gmail.com>
2023-03-03 12:56:22 +0200

    Comment card style with user icon

--- src/components/styles/CommentCard.styled.jsx
+++ src/components/styles/CommentCard.styled.jsx
@@ -1,22 +1,52 @@
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import styled from 'styled-components';
 
+export const UserIcon = () => {
+  return (
+    <IconDiv>
+      <AccountCircleIcon />
+    </IconDiv>
+  );
+};
+
+const IconDiv = styled.div`
+  align-items: center;
+
+  svg {
+    font-size: 38px;
+  }
+`;
+
+export const UserContent = styled.div`
+  display: flex;
+  gap: 5px;
+`;
+
+export const UserInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 export const CardContainer = styled.div`
   display: flex;
   flex-direction: column;
-  border: 1px solid black;
+  /* border: 1px solid lightgray; */
+  padding: 10px;
 `;
 
 export const UserName = styled.p`
   font-size: 20px;
+  margin: 0;
 `;
 
 export const Date = styled.p`
-  font-size: 10px;
+  font-size: 12px;
   margin: 0;
 `;
 
 export const Comment = styled.p`
-  font-size: 18px;
+  font-size: 20px;
+  padding-left: 10px;
+  margin-bottom: 10px;
 `;
 
 export const CommentsWrapper = styled.div`
