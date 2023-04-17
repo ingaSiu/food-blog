0cafffafce770a5587b8584bc2fed20c92a7620c
ingaSiu <ingasiud@gmail.com>
2023-04-17 13:39:56 +0300

    rating funcionality added

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,12 +1,38 @@
 import Rating from '@mui/material/Rating';
 import styled from 'styled-components';
+import toast from 'react-hot-toast';
+import { useRatePost } from '../hooks/posts.js';
+import { useState } from 'react';
 
-const StarRating = ({ ratingCount }) => {
+const StarRating = ({ ratingCount, rating, id }) => {
+  const [value, setValue] = useState(1);
+
+  const { mutateAsync: ratePost } = useRatePost();
+
+  const handleRating = (id, stars) => {
+    console.log(stars);
+    console.log(value);
+    ratePost({ id, stars })
+      .then(() => {
+        setValue(stars);
+        toast.succes('Recipe rated!');
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <RatingWrapper>
       <p>Rate this recipe!</p>
       <RatingContainer>
-        <Rating name="size-large" defaultValue={1} size="large" />
+        <Rating
+          name="size-large"
+          defaultValue={1}
+          size="large"
+          value={value}
+          onChange={(event, newValue) => {
+            handleRating(id, newValue);
+          }}
+        />
         {/* <Rating
           name="simple-controlled"
           //value={value}
@@ -14,7 +40,9 @@ const StarRating = ({ ratingCount }) => {
     setValue(newValue);
   }}
         /> */}
-        <p>Total score: {ratingCount}</p>
+        <p>
+          Total score: {rating} from {ratingCount} users
+        </p>
       </RatingContainer>
     </RatingWrapper>
   );
