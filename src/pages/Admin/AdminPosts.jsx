3dbdb0fc075fc8eb5011156ea43fd15b6d14064e
ingaSiu <ingasiud@gmail.com>
2023-04-25 19:55:58 +0300

    add useStates for opening edit form modal

--- src/pages/Admin/AdminPosts.jsx
+++ src/pages/Admin/AdminPosts.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react';
 import { TableWrapper, TitleContainer } from '../../components/AdminPageTables/Table.styled';
-import CreateNewPost from '../../components/AdminPageForms/PostPostsForm';
-import { TablePageContainer } from '../../components/AdminPageTables/Table.styled';
-import PostsTable from '../../components/AdminPageTables/PostsTable';
+
 import ButtonMain from '../../components/Button';
+import CreateNewPost from '../../components/AdminPageForms/PostPostsForm';
+import PostsTable from '../../components/AdminPageTables/PostsTable';
+import { TablePageContainer } from '../../components/AdminPageTables/Table.styled';
+import { useState } from 'react';
 
 const AdminPosts = () => {
   const [openForm, setOpenForm] = useState(false);
+  const [post, setPost] = useState(null);
+  const [postId, setPostId] = useState(null);
+
+  const openModal = (postInFnc, postIdInFnc) => {
+    setOpenForm(true);
+    setPost(postInFnc);
+    setPostId(postIdInFnc);
+  };
 
   return (
     <TablePageContainer>
@@ -15,17 +24,17 @@ const AdminPosts = () => {
           <h1>All blog posts</h1>
           <ButtonMain
             onClick={() => {
-              setOpenForm(true);
+              openModal();
             }}
           >
             Create a new post
           </ButtonMain>
         </TitleContainer>
 
-        <PostsTable></PostsTable>
+        <PostsTable openEditModal={openModal}></PostsTable>
       </TableWrapper>
 
-      {openForm && <CreateNewPost closeForm={setOpenForm} />}
+      {openForm && <CreateNewPost closeForm={setOpenForm} post={post} postId={postId} />}
     </TablePageContainer>
   );
 };
