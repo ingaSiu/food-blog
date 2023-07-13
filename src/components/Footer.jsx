ee900936b70e93674049b064dd17fa70bf9788de
ingaSiu <ingasiud@gmail.com>
2023-07-13 19:16:59 +0300

    small fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,8 @@
 import { FooterContainer, FooterText } from './styles/Footer.styled';
 
 const Footer = () => {
-  return (
-    <FooterContainer>
-      <FooterText>COPYRIGHT © 2023 NEKOTASTIC KITCHEN</FooterText>
-    </FooterContainer>
-  );
+  <FooterContainer>
+    <FooterText>COPYRIGHT © 2023 NEKOTASTIC KITCHEN</FooterText>
+  </FooterContainer>;
 };
 export default Footer;
