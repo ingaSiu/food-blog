bf4877adebc9e4ceba14543fd4572798925b4f59
ingaSiu <ingasiud@gmail.com>
2023-08-03 16:36:17 +0300

    mobile menu styled components

---
+++ src/components/MobileMenu/MobileMenu.styled.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const SlidingMenu = styled.div`
+  position: absolute;
+  top: 2.8rem;
+  left: 0;
+  right: 0;
+  height: 0;
+  background-color: white;
+  transition: height 0.3s ease-in-out;
+  overflow: hidden;
+`;
