f988f8c03e67229eb984b183fa525c1ad3e0a6f3
ingaSiu <ingasiud@gmail.com>
2023-08-18 19:25:00 +0300

    fixed phone resp

--- src/components/styles/CommentCard.styled.jsx
+++ src/components/styles/CommentCard.styled.jsx
@@ -54,11 +54,10 @@ export const CommentsWrapper = styled.div`
   flex-direction: column;
   gap: 10px;
 
-  @media (max-width: 400px) {
-    width: 30%;
-  }
-
   @media (max-width: 768px) {
     width: 60%;
+  }
+  @media (max-width: 400px) {
+    width: 50%;
   }
 `;
